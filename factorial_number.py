1813d8c66b94abda9e56d710a30d4f5ec68f7e46
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-10-25 16:43:36 +0000

    factorial number

---
+++ factorial_number.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Created by: Teddy Sannan
+# Created on: October 2019
+# This program shows the factorial of a number
+
+
+def main():
+    while True:
+        # input
+        multiplied_number = 1
+        total_number = 1
+        number = input("Input the number: ")
+        print()
+
+        try:
+            number_as_number = int(number)
+            while number_as_number > multiplied_number:
+                # process
+                print("x{0}".format(multiplied_number))
+                multiplied_number = multiplied_number + 1
+                total_number = multiplied_number * total_number
+
+            print("x{0}".format(number_as_number))
+            print("The answer is {0}".format(total_number))
+
+        except ValueError:
+            print()
+            print("Invalid Input")
+            print()
+            continue
+
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
